42c0dc605f8a682f5404a662b5b520178d30b60d
Michael Lynch <git@mtlynch.io>
2023-11-17 20:45:31 -0500

    Refactor codes_test

--- server/codes/codes_test.go
+++ server/codes/codes_test.go
@@ -1,34 +1,66 @@
 package codes_test
 
 import (
-	"runtime"
+	"fmt"
 	"testing"
 
 	"github.com/creikey/rpgpt/server/codes"
 )
 
-func assert(t *testing.T, cond bool) {
-	if !cond {
-		_, _, line, _ := runtime.Caller(1)
-		t.Fatalf("Failed on line %d", line)
+func TestParseUserCode(t *testing.T) {
+	for _, tt := range []struct {
+		input string
+		code  codes.UserCode
+		err   error
+	}{
+		{
+			input: "AAAA",
+			code:  codes.UserCode(0),
+			err:   nil,
+		},
+		{
+			input: "AAAB",
+			code:  codes.UserCode(1),
+			err:   nil,
+		},
+		{
+			input: "BAAA",
+			code:  codes.UserCode(46656),
+			err:   nil,
+		},
+	} {
+		t.Run(tt.input, func(t *testing.T) {
+			code, err := codes.ParseUserCode(tt.input)
+			if got, want := err, tt.err; got != want {
+				t.Fatalf("err=%v, want=%v", got, want)
+			}
+			if got, want := code, tt.code; got != want {
+				t.Errorf("code=%v, want=%v", got, want)
+			}
+		})
 	}
 }
 
-func TestCodes(t *testing.T) {
-	parsed, err := codes.ParseUserCode("AAAA")
-	assert(t, err == nil)
-	assert(t, int(parsed) == 0)
-
-	var stringed string
-	stringed, err = codes.CodeToString(codes.UserCode(1))
-	assert(t, err == nil)
-	assert(t, stringed == "AAAB")
-
-	parsed, err = codes.ParseUserCode("AAAB")
-	assert(t, err == nil)
-	assert(t, int(parsed) == 1)
-
-	parsed, err = codes.ParseUserCode("BAAA")
-	assert(t, err == nil)
-	assert(t, int(parsed) == 46656)
+func TestCodeToString(t *testing.T) {
+	for _, tt := range []struct {
+		input   codes.UserCode
+		encoded string
+		err     error
+	}{
+		{
+			input:   codes.UserCode(1),
+			encoded: "AAAB",
+			err:     nil,
+		},
+	} {
+		t.Run(fmt.Sprintf("%v", tt.input), func(t *testing.T) {
+			encoded, err := codes.CodeToString(tt.input)
+			if got, want := err, tt.err; got != want {
+				t.Fatalf("err=%v, want=%v", got, want)
+			}
+			if got, want := encoded, tt.encoded; got != want {
+				t.Errorf("encoded=%v, want=%v", got, want)
+			}
+		})
+	}
 }
